62e74dd3529dd5ce7517220a91db8e45d6ae7411
Ariel Sil <themoil77@gmail.com>
2024-11-26 19:24:48 +0100

    Ajout de BlueSky

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import { TwitterApi } from "twitter-api-v2";
 import express from 'express';
 import { CronJob } from "cron";
 import { format, getISOWeek } from "date-fns";
+import bsky from "@atproto/api";
+
+const { BskyAgent } = bsky;
+
 
 const drapeaux = { "United Kingdom": "🇬🇧", "United States": "🇺🇸", "Australia": "🇦🇺", "Germany": "🇩🇪", "Canada": "🇨🇦" };
 const emoji_fr = ["🥖", "🍷", "🧀", "🥐"];
@@ -36,9 +40,12 @@ const client = new TwitterApi({
 const bearer = new TwitterApi(process.env.BEARER_TOKEN);
 
 const twitterClient = client.readWrite;
-const twitterBearer = bearer.readOnly;
 const key_ytb = process.env.API_KEY_YTB;
-
+const agent = new BskyAgent({
+    service: 'https://bsky.social',
+});
+await agent.login({ identifier: process.env.BLUESKY_USERNAME,
+    password: process.env.BLUESKY_PASSWORD });
 
 const main = async () => {
     const date = new Date();
@@ -58,20 +65,21 @@ const main = async () => {
     const year = getRandomIntInclusive(1960, 2023).toString();
 
     const chiffre = getRandomIntInclusive(0, 6);
+    const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+            '--disable-features=site-per-process',
+        ],
+        executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+    });
 
     
 
         if (chiffre != 5 && chiffre != 6) {
-            const browser = await puppeteer.launch({
-                args: [
-                    "--disable-setuid-sandbox",
-                    "--no-sandbox",
-                    "--single-process",
-                    "--no-zygote",
-                    '--disable-features=site-per-process',
-                ],
-                executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-            });
+
             const page = await browser.newPage();
             await page.goto(url,{ waitUntil: 'networkidle2', timeout: 0 });
             await page.select("#day_start", day);
@@ -99,9 +107,13 @@ const main = async () => {
                 if (names[chiffre]['country'] == "United Kingdom" || names[chiffre]['country'] == "United States") {
                     twitterClient.v2.tweet(`🕰️ The ${day} of ${month} of ${year}, the most popular song in the ${names[chiffre]['country']} ${drapeaux[names[chiffre]['country']]} was '${names[chiffre]['song']}' by ${names[chiffre]['name']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`);
                     console.log("envoyer tweet");
+                    agent.post({'text':`🕰️ The ${day} of ${month} of ${year}, the most popular song in the ${names[chiffre]['country']} ${drapeaux[names[chiffre]['country']]} was '${names[chiffre]['song']}' by ${names[chiffre]['name']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`});
+                    console.log("envoyer papillon");
                 } else {
                     twitterClient.v2.tweet(`🕰️ The ${day} of ${month} of ${year}, the most popular song in ${names[chiffre]['country']} ${drapeaux[names[chiffre]['country']]} was '${names[chiffre]['song']}' by ${names[chiffre]['name']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`);
-                    console.log("envoyer tweet");
+                    console.log("envoyer tweet")
+                    agent.post({'text':`🕰️ The ${day} of ${month} of ${year}, the most popular song in the ${names[chiffre]['country']} ${drapeaux[names[chiffre]['country']]} was '${names[chiffre]['song']}' by ${names[chiffre]['name']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`});
+                    console.log("envoyer papillon");
                 }
             }).catch(error => alert('Error : ' + error));
 
@@ -111,16 +123,6 @@ const main = async () => {
             const year_fr = getRandomIntInclusive(1984, 2020).toString();
             const old_date = format(new Date(year_fr, month_nb, day), "yyyy-MM-dd");
             const weekNumber = getISOWeek(old_date);
-            const browser = await puppeteer.launch({
-                args: [
-                    "--disable-setuid-sandbox",
-                    "--no-sandbox",
-                    "--single-process",
-                    "--no-zygote",
-                    '--disable-features=site-per-process',
-                ],
-                executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-            });
             const page = await browser.newPage();
 
             if (weekNumber.length == 1) {
@@ -129,8 +131,9 @@ const main = async () => {
             } else {
                 await page.goto("https://www.chartsinfrance.net/charts/" + year_fr.slice(-2) + weekNumber + "/singles.php", { waitUntil: 'networkidle2', timeout: 0 });
             };
-            
-            
+            //await page.click("#cmp-main > button.jad_cmp_paywall_button.jad_cmp_paywall_button-cookies.jad_cmp_paywall_cookies.didomi-components-button.didomi-button.didomi-dismiss-button.didomi-components-button--color.didomi-button-highlight.highlight-button");
+            //await page.reload();
+            //await page.waitForSelector("#contenu > table > tbody > tr > td > div:nth-child(3) > div.b618 > div:nth-child(4) > div.c1_td1 > div.c1_td5", { waitUntil: 'load', timeout: 0 });
             const name = await page.evaluate(() => {
                 if (document.querySelector("#contenu > table > tbody > tr > td > div:nth-child(3) > div.b618 > div:nth-child(4) > div.c1_td1 > div.c1_td5 > font.noir13b > a") !== null) {
                     const cadre_1 = document.querySelector("#contenu > table > tbody > tr > td > div:nth-child(3) > div.b618 > div:nth-child(4) > div.c1_td1 > div.c1_td5 > font.noir13b");
@@ -154,21 +157,13 @@ const main = async () => {
             fetch(url_ytb).then(response => response.json()).then(data => {
                 const id = data.items[0].id.videoId;
                 const chifrre_emoji = getRandomIntInclusive(0, 3);
-                twitterClient.v2.tweet("🕰️ " + "The " + day + " of " + month + " of " + year_fr + " (week " + weekNumber + ")" + ", the most popular song in " + "France" + " 🇫🇷 " + emoji_fr[chifrre_emoji % 4] + " " + emoji_fr[(chifrre_emoji + 1) % 4] + " " + emoji_fr[(chifrre_emoji + 2) % 4] + " was " + "'" + name['musique'] + "'" + " by " + name['nom'] +" #"+ hashtags[getRandomIntInclusive(0,2)] + " ." + `https://www.youtube.com/watch?v=${id}`);
+                twitterClient.v2.tweet(`🕰️ The ${day} ${month} of ${year_fr} (week ${weekNumber}), the most popular song in France 🇫🇷  ${emoji_fr[chifrre_emoji % 4]} ${emoji_fr[(chifrre_emoji + 1) % 4]} ${emoji_fr[(chifrre_emoji + 2) % 4]} was ${name['musique']} by ${name['nom']} #${hashtags[getRandomIntInclusive(0,2)]}. https://www.youtube.com/watch?v=${id}`);
                 console.log("envoyer tweet");
+                agent.post({'text':`🕰️ The ${day} ${month} of ${year_fr} (week ${weekNumber}), the most popular song in France 🇫🇷  ${emoji_fr[chifrre_emoji % 4]} ${emoji_fr[(chifrre_emoji + 1) % 4]} ${emoji_fr[(chifrre_emoji + 2) % 4]} was ${name['musique']} by ${name['nom']} #${hashtags[getRandomIntInclusive(0,2)]}. https://www.youtube.com/watch?v=${id}`});
+                console.log("envoyer papillon");        
             }).catch(error => alert('Error : ' + error));
 }; if (chiffre == 6) {
     const year_jap = getRandomIntInclusive(2014, 2023).toString(); 
-    const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-            '--disable-features=site-per-process',
-        ],
-        executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-    });
     const page = await browser.newPage();
     await page.goto(`https://kma.kkbox.com/charts/daily/song?cate=308&date=${year_jap}-${month_url}-${day}&lang=en&terr=sg`,{
     waitUntil: 'networkidle0', timeout: 0 
@@ -188,13 +183,14 @@ fetch(url_ytb).then(response => response.json()).then(data => {
     const chifrre_emoji = getRandomIntInclusive(0, 3);
     twitterClient.v2.tweet(`🕰️ The ${day} of ${month} of ${year_jap}, the most popular song in Japan 🇯🇵 ${emoji_jap[chifrre_emoji % 4]} ${emoji_jap[(chifrre_emoji + 1) % 4]} ${emoji_jap[(chifrre_emoji + 2) % 4]} was '${cadre['musique']}' by ${cadre['artiste']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`);
     console.log("envoyer tweet");
+    agent.post({'test':`🕰️ The ${day} of ${month} of ${year_jap}, the most popular song in Japan 🇯🇵 ${emoji_jap[chifrre_emoji % 4]} ${emoji_jap[(chifrre_emoji + 1) % 4]} ${emoji_jap[(chifrre_emoji + 2) % 4]} was '${cadre['musique']}' by ${cadre['artiste']} #${hashtags[getRandomIntInclusive(0,2)]} . https://www.youtube.com/watch?v=${id}`});
+    console.log("envoyer papillon");
 }).catch(error => alert('Error : ' + error));
 
 
 
 };
-};
-
+    };
 
 
 
@@ -205,16 +201,16 @@ app.get('/', function (req, res) {
 
 
 
-
 const job = new CronJob(
-    "0 */2 * * *", // cronTime
+    "0 */2 * * *", // Le refaire
     function () {
         try{
-        main();}catch(e){main();} // trop d'appels récursifs font crasher l'API de X
+        main();}catch(e){main();} //Les appels récursifs font crasher l'API de X
     }, // onTick
     console.log("ça va tweeter"), // onComplete
     true, // start
-);
+); 
+
 
 
 
